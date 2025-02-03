b9031c91329126e42de4c854eefb220f3ecc409e
codal-mchandarana <mchandarana@codal.com>
2025-02-03 23:03:49 +0530

    Fix: #5 Fixed the issue of vercel deployement

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.json$/,
-      use: 'json-loader',
-    });
-    return config;
-  },
   images: {
     remotePatterns: [
       {
--- src/pages/ProductListing.tsx
+++ src/pages/ProductListing.tsx
@@ -1,6 +1,6 @@
 import type { JSX } from 'react';
 import ProductCard from '@/components/Custom/products/ProductCard';
-import { products } from '@/data/data.json';
+import data from '@/data/data.json';
 import type { Product } from '@/lib/types';
 
 const ProductListing: React.FC = (): JSX.Element => {
@@ -8,7 +8,7 @@ const ProductListing: React.FC = (): JSX.Element => {
     <section className="mt-12 py-8 antialiased dark:bg-gray-900 md:py-12">
       <div className="mx-auto px-4 xl:max-w-screen-xl 2xl:max-w-screen-2xl 2xl:px-0">
         <div className="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4">
-          {products.map((product): JSX.Element => {
+          {data.products.map((product): JSX.Element => {
             const p = product as Product;
             return <ProductCard key={p.id} product={p} />;
           })}
