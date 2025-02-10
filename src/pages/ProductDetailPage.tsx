db32f9b5331b1981ca86870be38a0b153b8f7abd
codal-mchandarana <mchandarana@codal.com>
2025-02-10 23:53:11 +0530

    Fix : #8 Fixed the issue of responsive ness

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -43,13 +43,13 @@ const ProductDetailPage: React.FC = (): JSX.Element => {
   };
 
   return (
-    <div className="mt-10 h-screen bg-transparent">
+    <div className="mt-10 h-screen overflow-auto bg-transparent">
       <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+        <div className="grid grid-cols-1 gap-16 md:grid-cols-2">
           {/* Product Images Section */}
           <div className="space-y-4">
             {/* Main Image */}
-            <div className="aspect-square overflow-hidden rounded-lg">
+            <div className="aspect-square overflow-hidden rounded-lg shadow-[0px_5px_15px_rgba(0,0,0,0.35)]">
               <Image
                 src={productData?.images[selectedImage].src ?? ''}
                 alt="Product"
@@ -119,7 +119,7 @@ const ProductDetailPage: React.FC = (): JSX.Element => {
             <p className="text-gray-600">{product.description}</p>
 
             {/* Tags Dropdown */}
-            <div className='w-1/4'>
+            <div className='w-1/4 min-w-fit'>
               <label
                 htmlFor="tag"
                 className="block text-sm font-medium text-gray-700"
